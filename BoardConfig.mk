96009b43b9c1e0c131a47a2b40bc98f69889fb86
Akshat Saroha <79436731+akshat942@users.noreply.github.com>
2021-10-12 21:19:39 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -144,6 +144,8 @@ TARGET_USES_MKE2FS := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
